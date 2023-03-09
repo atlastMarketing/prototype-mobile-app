90c7f7be2ae7053577f82cda6bcb2909b2ef3322
Clarence Adrian <44561376+ad2969@users.noreply.github.com>
2023-03-08 18:35:22 -0800

    implement upcoming posts on dashboard

--- lib/screens/home/home_dashboard.dart
+++ lib/screens/home/home_dashboard.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
-import 'package:atlast_mobile_app/configs/layout.dart';
 import 'package:atlast_mobile_app/configs/theme.dart';
 import 'package:atlast_mobile_app/constants/suggested_posts.dart';
+import 'package:atlast_mobile_app/data/upcoming_posts.dart';
+
+import 'package:atlast_mobile_app/shared/avatar_image.dart';
 import 'package:atlast_mobile_app/shared/button.dart';
 import 'package:atlast_mobile_app/shared/hero_heading.dart';
 import 'package:atlast_mobile_app/shared/post_preview.dart';
+import 'package:atlast_mobile_app/shared/layouts/full_page.dart';
+import 'package:atlast_mobile_app/shared/layouts/single_child_scroll_bare.dart';
 
 class HomeDashboard extends StatelessWidget {
   final GlobalKey<NavigatorState> navKey;
@@ -17,12 +23,15 @@ class HomeDashboard extends StatelessWidget {
     required this.handleCreate,
   }) : super(key: key);
 
+  void _openUpcomingPost(int postId) {
+    print("Opening upcoming post iwth id $postId");
+  }
+
   Widget _buildSuggestedPosts() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text("Today's Suggested Posts", style: AppText.subheading),
-        const Padding(padding: EdgeInsets.only(bottom: 8)),
         Row(
           children: [
             const Icon(Icons.auto_awesome, size: 10, color: AppColors.primary),
@@ -60,71 +69,154 @@ class HomeDashboard extends StatelessWidget {
   }
 
   Widget _buildUpcomingPosts() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TextButton(
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(const EdgeInsets.all(0)),
-          ),
-          onPressed: () {},
-          child: Row(
-            children: [
-              Text(
-                "Upcoming Posts",
-                style: AppText.subheading.merge(AppText.blackText),
-              ),
-              const Icon(Icons.chevron_right, color: AppColors.black),
-            ],
-          ),
-        ),
-        const Padding(padding: EdgeInsets.only(bottom: 8)),
-        Material(
-          elevation: 1,
-          child: Container(
-            height: 100,
-            width: double.infinity,
-            color: AppColors.dark.withOpacity(0.1),
-            child: const Center(
-              child: Text("There's nothing scheduled!"),
+    return Consumer<UpcomingPostsModel>(
+      builder: (context, model, child) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextButton(
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(const EdgeInsets.all(0)),
+            ),
+            onPressed: () {},
+            child: Row(
+              children: [
+                Text(
+                  "Upcoming Posts",
+                  style: AppText.subheading.merge(AppText.blackText),
+                ),
+                const Icon(Icons.chevron_right, color: AppColors.black),
+              ],
             ),
           ),
-        )
-      ],
+          model.posts.isNotEmpty
+              ? Column(
+                  children: model.posts.map((post) {
+                    DateTime? dt = post.dateTime != null
+                        ? DateTime.tryParse(post.dateTime!)
+                        : null;
+                    String dtStrMonth = dt != null
+                        ? DateFormat.MMM().format(dt).toUpperCase()
+                        : "??";
+                    String dtStrDate =
+                        dt != null ? DateFormat.d().format(dt) : "??";
+
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: SizedBox(
+                        height: 100,
+                        width: double.infinity,
+                        child: Material(
+                          color: AppColors.light,
+                          borderRadius: BorderRadius.circular(10),
+                          child: InkWell(
+                            onTap: () => _openUpcomingPost(post.id),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20,
+                                horizontal: 15,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        dtStrMonth,
+                                        style: AppText.bodySemiBold,
+                                      ),
+                                      Text(
+                                        dtStrDate,
+                                        style: AppText.heading,
+                                      ),
+                                    ],
+                                  ),
+                                  const Padding(
+                                      padding: EdgeInsets.only(right: 15)),
+                                  Container(
+                                    decoration: const BoxDecoration(
+                                        color: AppColors.black),
+                                    height: 100,
+                                    width: 1,
+                                  ),
+                                  const Padding(
+                                      padding: EdgeInsets.only(right: 15)),
+                                  Flexible(
+                                    child: Text(
+                                      post.caption ?? "No caption yet!",
+                                      style: post.caption != null
+                                          ? AppText.bodySmall
+                                          : AppText.bodySmall
+                                              .merge(AppText.primaryText),
+                                      maxLines: 4,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const Padding(
+                                      padding: EdgeInsets.only(right: 20)),
+                                  AvatarImage(
+                                    post.imageUrl ?? "",
+                                    width: 60,
+                                    height: 60,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                )
+              : Material(
+                  elevation: 1,
+                  child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    color: AppColors.dark.withOpacity(0.1),
+                    child: const Center(
+                      child: Text(
+                        "There's nothing scheduled!",
+                        style: AppText.bodySmall,
+                      ),
+                    ),
+                  ),
+                )
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: pagePadding,
-          child: Column(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const HeroHeading(text: "Dashboard"),
-                    _buildSuggestedPosts(),
-                    const Padding(padding: EdgeInsets.only(bottom: 40)),
-                    _buildUpcomingPosts(),
-                  ],
-                ),
+    return LayoutFullPage(
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const HeroHeading(text: "Dashboard"),
+          Expanded(
+            child: SingleChildScrollBare(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _buildSuggestedPosts(),
+                  const Padding(padding: EdgeInsets.only(bottom: 20)),
+                  _buildUpcomingPosts(),
+                ],
               ),
-              SizedBox(
-                width: double.infinity,
-                child: CustomButton(
-                  text: 'Create',
-                  handlePressed: handleCreate,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-        extendBody: false,
+          SizedBox(
+            width: double.infinity,
+            child: CustomButton(
+              text: 'Create',
+              handlePressed: handleCreate,
+            ),
+          ),
+        ],
       ),
     );
   }
