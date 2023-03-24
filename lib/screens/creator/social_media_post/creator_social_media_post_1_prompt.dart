a34cfb93aa40385940d989ff93ff4d2fde227892
Clarence Adrian <44561376+ad2969@users.noreply.github.com>
2023-03-23 19:31:27 -0700

    move annotations out to its own model (abstract)

--- lib/screens/creator/social_media_post/creator_social_media_post_1_prompt.dart
+++ lib/screens/creator/social_media_post/creator_social_media_post_1_prompt.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:atlast_mobile_app/configs/theme.dart';
 import 'package:atlast_mobile_app/constants/catalyst_output_types.dart';
 import 'package:atlast_mobile_app/constants/social_media_platforms.dart';
+import 'package:atlast_mobile_app/models/annotations_model.dart';
 import 'package:atlast_mobile_app/models/catalyst_model.dart';
 import 'package:atlast_mobile_app/shared/annotated_text_field.dart';
 import 'package:atlast_mobile_app/shared/app_bar_steps.dart';
