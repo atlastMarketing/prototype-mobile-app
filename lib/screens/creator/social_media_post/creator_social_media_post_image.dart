fe3fc46b97f3d3b6060f0614e40bdcfdc733905f
401asfg <mb.allan@ymail.com>
2023-03-25 21:08:07 -0700

    Made ai assistant message for image taking more concise; added back arrow and flow process bar to top of image taking screen

--- lib/screens/creator/social_media_post/creator_social_media_post_image.dart
+++ lib/screens/creator/social_media_post/creator_social_media_post_image.dart
@@ -7,6 +7,9 @@ import 'package:info_popup/info_popup.dart';
 
 import 'package:image_picker/image_picker.dart';
 
+import '../../../shared/app_bar_steps.dart';
+import '../../../shared/layouts/full_page.dart';
+
 class CreatorSocialMediaPostImage extends StatefulWidget {
   final GlobalKey<NavigatorState> navKey;
   final Function(String) saveImageUrl;
@@ -26,6 +29,11 @@ class _CreatorSocialMediaPostImageState
     extends State<CreatorSocialMediaPostImage> {
   File? imageFile;
   bool infoPopupDismissed = false;
+  final _formKey = GlobalKey<FormState>();
+
+  void _handleBack() {
+    widget.navKey.currentState!.pop();
+  }
 
   void _getFromCamera() async {
     XFile? pickedFile = await ImagePicker().pickImage(
@@ -45,48 +53,48 @@ class _CreatorSocialMediaPostImageState
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          imageFile != null
-              ? Image.file(imageFile!)
-              : Icon(
-                  Icons.camera_enhance_rounded,
-                  color: AppColors.secondary,
-                  size: MediaQuery.of(context).size.width * .6,
-                ),
-          Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: ElevatedButton(
-                  onPressed: () {
-                    _getFromCamera();
-                  },
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(AppColors.primary),
-                      padding:
-                          MaterialStateProperty.all(const EdgeInsets.all(20)),
-                      textStyle:
-                          MaterialStateProperty.all(AppText.buttonLargeText)),
-                  child: infoPopupDismissed == false
-                      ? InfoPopupWidget(
-                          onControllerCreated:
-                              (InfoPopupController controller) {
-                            controller.show();
-                          },
-                          infoPopupDismissed: () {
-                            setState(() => infoPopupDismissed = true);
-                          },
-                          contentTitle:
-                              "1. Use good lighting: Ensure the product is well-lit and avoid harsh shadows or glares.\n2. Focus on the product: Keep the focus on the product and avoid cluttered backgrounds or distracting elements.\n3. Use a visually appealing composition: Experiment with different angles and compositions to find the most attractive way to showcase the product.\n4. Showcase product benefits: Highlight the product's unique features and benefits in the caption or description.\n5. Keep it consistent: Maintain a consistent visual style across all your social media posts to create a cohesive brand image.\n\n- Your AI Marketing Advisor",
-                          enableHighlight: true,
-                          child: const Text('Take a Picture for your Post'))
-                      : const Text('Take a Picture for your Post')))
-        ],
-      ),
+    return LayoutFullPage(
+      handleBack: _handleBack,
+      appBarContent: const AppBarSteps(totalSteps: 2, currStep: 2),
+      content: Scaffold(
+          body: ListView(children: [
+        const SizedBox(
+          height: 50,
+        ),
+        imageFile != null
+            ? Image.file(imageFile!)
+            : Icon(
+                Icons.camera_enhance_rounded,
+                color: AppColors.secondary,
+                size: MediaQuery.of(context).size.width * .6,
+              ),
+        Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  _getFromCamera();
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(AppColors.primary),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(20)),
+                    textStyle:
+                        MaterialStateProperty.all(AppText.buttonLargeText)),
+                child: infoPopupDismissed == false
+                    ? InfoPopupWidget(
+                        onControllerCreated: (InfoPopupController controller) {
+                          controller.show();
+                        },
+                        infoPopupDismissed: () {
+                          setState(() => infoPopupDismissed = true);
+                        },
+                        contentTitle:
+                            "1. Ensure the product is well-lit and avoid harsh shadows or glares.\n2. Avoid cluttered backgrounds or distracting elements.\n3. Try different angles and compositions to find the most attractive way to showcase the product.\n4. Maintain a consistent visual style across all your social media posts.\n\n- Your AI Marketing Advisor",
+                        enableHighlight: true,
+                        child: const Text('Take a Picture for your Post'))
+                    : const Text('Take a Picture for your Post')))
+      ])),
     );
   }
 }
