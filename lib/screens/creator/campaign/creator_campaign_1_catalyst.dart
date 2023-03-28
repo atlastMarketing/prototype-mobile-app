9b1a565b8b76f4b325a908ffa7d4a5d0e71fd884
401asfg <mb.allan@ymail.com>
2023-03-28 12:09:34 -0700

    Attributed some of the help popup's to the ai marketing assistant

--- lib/screens/creator/campaign/creator_campaign_1_catalyst.dart
+++ lib/screens/creator/campaign/creator_campaign_1_catalyst.dart
@@ -169,7 +169,7 @@ class _CreatorCampaignCatalystState extends State<CreatorCampaignCatalyst> {
             child: HelpPopup(
               title: "Describe your campaign!",
               content:
-                  "Using as much detail, describe your campaign (including dates, frequency, platforms, etc). We'll convert everything you type here into meaningful information for your campaign!",
+                  "Using as much detail, describe your campaign (including dates, frequency, platforms, etc). We'll convert everything you type here into meaningful information for your campaign!\n\n- Your AI Marketing Assistant",
               child: CustomFormTextField(
                 controller: _catalystInputController,
                 placeholderText:
